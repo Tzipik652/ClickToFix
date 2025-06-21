f659095412bc9dcd2d3a02819da958e09c9ccaa5
Tzipik652 <tzipik652@gmail.com>
2025-06-22 01:18:40 +0300

    chore: initialize Spring Boot project with basic config

---
+++ src/main/java/com/technicians/clicktofix/ClicktofixApplication.java
@@ -0,0 +1,13 @@
+package com.technicians.clicktofix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicktofixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClicktofixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technicians/clicktofix/ClicktofixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicians.clicktofix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicktofixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
